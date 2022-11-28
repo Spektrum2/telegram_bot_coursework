df277b76ebeacf6ea39d03833b7707933be420eb
admin <promitay@gmail.com>
2022-11-28 21:57:21 +0300

    Курсовая работа 3 этапа (3-4-5 курсы)

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,21 +3,33 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final NotificationTaskRepository notificationTaskRepository;
 
     @Autowired
     private TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository) {
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
 
     @PostConstruct
     public void init() {
@@ -28,9 +40,44 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message() != null && update.message().text().equals("/start")) {
+                telegramBot.execute(new SendMessage(update.message().chat().id(), "Hello"));
+            } else if (update.message() != null && update.message().text().matches("([0-9.:\\s]{16})(\\s)([\\W+]+)")) {
+                processing(update.message().text(), update.message().chat().id());
+            } else {
+                telegramBot.execute(new SendMessage(update.message().chat().id(), "Неправильно введено сообщение! Шаблон сообщения: 01.01.2022 20:00 Сделать домашнюю работу"));
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
+    public void save(NotificationTask notificationTask) {
+        logger.info("Saving the notification to the database");
+        notificationTaskRepository.save(notificationTask);
+    }
+
+    public NotificationTask create(long chatId, String message, String date) {
+        logger.info("Filling in notification fields");
+        NotificationTask notificationTask = new NotificationTask();
+        notificationTask.setChatId(chatId);
+        notificationTask.setMessage(message);
+        notificationTask.setDate(LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+        return notificationTask;
+    }
+
+    public void processing(String message, long chatId) {
+        logger.info("Splitting a notification into a date and a message");
+        String date = message.substring(0, 16);
+        String item = message.substring(17);
+        save(create(chatId, item, date));
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void findByDate() {
+        logger.info("Sending notifications to users at a specified time");
+        List<NotificationTask> notificationTasks = notificationTaskRepository.findByDate(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
+        notificationTasks.forEach(user -> telegramBot.execute(new SendMessage(user.getChatId(), user.getMessage())));
+    }
+
 
 }
---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,49 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private long chatId;
+    private String message;
+    private LocalDateTime date;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
