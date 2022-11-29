e6a62c5cb3e57eec2101966b4d9e6c359acc74a8
admin <promitay@gmail.com>
2022-11-29 19:47:24 +0300

    Курсовая работа 3 этапа (3-4-5 курсы)

--- src/main/resources/liquibase/scripts/bot.sql
+++ src/main/resources/liquibase/scripts/bot.sql
@@ -5,7 +5,7 @@
 -- precondition-sql-check expectedResult:0 SELECT count(*) FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'notification_task'
 CREATE TABLE notification_task
 (
-    id      SERIAL PRIMARY KEY,
+    id      BIGSERIAL PRIMARY KEY,
     chat_id BIGINT,
     message TEXT,
     date    TIMESTAMP
